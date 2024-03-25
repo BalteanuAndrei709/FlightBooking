3a3fa68dd5cdf122410cc2c5c036bac95e1ee68b
Balteanu Andrei <andreibalteanu2@gmail.com>
2024-03-25 14:53:53 +0200

    Initial project setup for Admin and Tarom service

---
+++ TaromService/src/main/java/com/taromservice/TaromServiceApplication.java
@@ -0,0 +1,13 @@
+package com.taromservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaromServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaromServiceApplication.class, args);
+    }
+
+}
---
+++ TaromService/src/test/java/com/taromservice/TaromServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taromservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaromServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
