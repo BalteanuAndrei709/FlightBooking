599d5066bd037ee6b1732c4c0941d1030bb25754
Balteanu Andrei <andreibalteanu2@gmail.com>
2024-04-08 12:36:58 +0300

    Refactoring structure

--- OperatorService/src/main/java/com/operatorservice/model/Flight.java
+++ OperatorService/src/main/java/com/operatorservice/model/Flight.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDate;
 import java.util.Objects;
 
-@Document(value = "flight_sharding_leaving")
+@Document(value = "flight")
 public class Flight {
 
     @Id
