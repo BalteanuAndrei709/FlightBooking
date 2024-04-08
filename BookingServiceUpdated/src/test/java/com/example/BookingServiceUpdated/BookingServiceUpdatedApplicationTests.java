2a6c7b28534eb446de0d821de0456438713c9f88
Matei <matei.cozac@gmail.com>
2024-04-08 15:46:25 +0300

    Rest of files

---
+++ BookingServiceUpdated/src/test/java/com/example/BookingServiceUpdated/BookingServiceUpdatedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookingServiceUpdated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceUpdatedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
