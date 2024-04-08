9d494e6acc033de16fea0cbd0f47f6a2a01967fc
Balteanu Andrei <andreibalteanu2@gmail.com>
2024-04-08 17:16:05 +0300

    Modified the Unit Test for operator search flight to adapt the new pagination

--- OperatorService/src/test/java/com/operatorservice/UnitTests/FlightController/SearchFlightsTest.java
+++ OperatorService/src/test/java/com/operatorservice/UnitTests/FlightController/SearchFlightsTest.java
@@ -33,46 +33,48 @@ public class SearchFlightsTest {
     private WebTestClient webClient;
     String url = "/api/v1.0/flights";
 
-//    @Test
-//    void testSearchFlightAllParams(){
-//
-//        String leaving = "Iasi";
-//        String destination = "London";
-//        LocalDate departureDate = LocalDate.parse("2024-05-13");
-//        LocalDate returnDate = LocalDate.parse("2024-05-15");
-//
-//        Flight leavingFlight1 = new Flight("660be61e202736b8ad10b1e4", "WizzAir", departureDate,
-//                "02:49:12", "Krakow", "London",  7.57f);
-//        List<Flight> leavingFlights = List.of(leavingFlight1);
-//
-//        Flight returningFlight = new Flight("660be689202736b8ad13c93d", "WizzAir", returnDate,
-//                "00:54:50", "London", "Krakow",  2.64f);
-//        List<Flight> returningFlights =  List.of(returningFlight);
-//
-//        Mono<SearchFlightResponseDto> response = Mono.just(new SearchFlightResponseDto(leavingFlights, returningFlights));
-//
-//        Mockito.when(flightService.searchFlight(leaving, destination, departureDate, returnDate, 3, 3)).thenReturn(response);
-//
-//        webClient
-//                .get()
-//                .uri(uriBuilder -> uriBuilder
-//                        .path(url + "/" + leaving)
-//                        .queryParam("destination", destination)
-//                        .queryParam("departureDate", departureDate)
-//                        .queryParam("returnDate", returnDate)
-//                        .build())
-//                .exchange()
-//                .expectStatus().isOk()
-//                .expectBody(SearchFlightResponseDto.class)
-//                .consumeWith(result -> {
-//                    var responseBody = result.getResponseBody();
-//
-//                    assert responseBody != null;
-//                    assert responseBody.getReturningFlight() != null;
-//                    assert responseBody.getLeavingFlight() != null;
-//
-//                    assertEquals(responseBody.getLeavingFlight(), leavingFlights);
-//                    assertEquals(responseBody.getReturningFlight(), returningFlights);
-//                });
-//        }
+    @Test
+    void testSearchFlightAllParams(){
+
+        String leaving = "Iasi";
+        String destination = "London";
+        LocalDate departureDate = LocalDate.parse("2024-05-13");
+        LocalDate returnDate = LocalDate.parse("2024-05-15");
+
+        Flight leavingFlight1 = new Flight("660be61e202736b8ad10b1e4", "WizzAir", departureDate,
+                "02:49:12", "Krakow", "London",  7.57f);
+        List<Flight> leavingFlights = List.of(leavingFlight1);
+
+        Flight returningFlight = new Flight("660be689202736b8ad13c93d", "WizzAir", returnDate,
+                "00:54:50", "London", "Krakow",  2.64f);
+        List<Flight> returningFlights =  List.of(returningFlight);
+
+        Mono<SearchFlightResponseDto> response = Mono.just(new SearchFlightResponseDto(leavingFlights, returningFlights));
+
+        Mockito.when(flightService.searchFlight(leaving, destination, departureDate, returnDate, 0, 1)).thenReturn(response);
+
+        webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(url + "/" + leaving)
+                        .queryParam("destination", destination)
+                        .queryParam("departureDate", departureDate)
+                        .queryParam("returnDate", returnDate)
+                        .queryParam("pageNumber", 0)
+                        .queryParam("pageSize", 1)
+                        .build())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(SearchFlightResponseDto.class)
+                .consumeWith(result -> {
+                    var responseBody = result.getResponseBody();
+
+                    assert responseBody != null;
+                    assert responseBody.getReturningFlight() != null;
+                    assert responseBody.getLeavingFlight() != null;
+
+                    assertEquals(responseBody.getLeavingFlight(), leavingFlights);
+                    assertEquals(responseBody.getReturningFlight(), returningFlights);
+                });
+        }
 }
